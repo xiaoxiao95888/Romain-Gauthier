dc56dc83a3cc1772329a65719758f12247c2772b
lock0 <xiaoxiao95888@163.com>
2016-04-01 21:48:38 +0800

    Romain-Gauthier

---
+++ Romain-Gauthier.Web/Romain-Gauthier.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Romain_Gauthier.Web.MvcApplication" Language="C#" %>
---
+++ Romain-Gauthier.Web/Romain-Gauthier.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Romain_Gauthier.Web.Startup))]
+namespace Romain_Gauthier.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

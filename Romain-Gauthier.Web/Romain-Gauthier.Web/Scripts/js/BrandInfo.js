92a35b321b1bb2b7f195287d5e4e8fa6409466d1
lock0 <xiaoxiao95888@163.com>
2016-04-06 01:06:41 +0800

    fix caption

--- Romain-Gauthier.Web/Romain-Gauthier.Web/Scripts/js/BrandInfo.js
+++ Romain-Gauthier.Web/Romain-Gauthier.Web/Scripts/js/BrandInfo.js
@@ -1,25 +1,38 @@
 ﻿$.fn.extend({
-    animateCss: function (animationName,callback) {
+    animateCss: function (animationName, callback) {
         var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
         $(this).addClass("animated " + animationName).one(animationEnd, function () {
             $(this).removeClass("animated " + animationName);
         });
     }
 });
-$(function() {
+
+$(function () {
     $("p").animateCss("fadeIn");
     //hover效果
-    $("header,section,footer,li").on("touchstart", function (e) {
-        "use strict"; 
-        var link = $(this); 
+    $("header,footer,.li-item").on("touchstart", function (e) {
+        "use strict";
+        var link = $(this);
         if (link.hasClass("hover")) {
             link.removeClass("hover");
             link.css("background-size", "100%");
+            $(link).find(".caption").fadeOut();
         } else {
             link.addClass("hover");
             link.css("background-size", "110%");
+            link.find(".caption").fadeIn();
+
+            for (var i = 0; i < $("header,footer,.li-item").length; i++) {
+                var other = $($("header,footer,.li-item")[i]);
+                if (link.is(other) == false && other.hasClass("hover")) {
+                    other.removeClass("hover");
+                    other.css("background-size", "100%");
+                    other.find(".caption").fadeOut();
+
+                }
+            }
             e.preventDefault();
-           
+
         }
     });
 });

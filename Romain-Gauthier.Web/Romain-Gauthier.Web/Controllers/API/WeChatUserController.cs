7c147dff38855813060b9f037174f02fda3d4fd5
lock0 <xiaoxiao95888@163.com>
2016-04-25 07:55:03 +0800

    漏文件

---
+++ Romain-Gauthier.Web/Romain-Gauthier.Web/Controllers/API/WeChatUserController.cs
@@ -0,0 +1,37 @@
+﻿using Romain_Gauthier.Web.Infrastructure;
+using Romain_Gauthier.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Romain_Gauthier.Web.Controllers.API
+{
+    public class WeChatUserController : ApiController
+    {
+        public object Get()
+        {
+            var openId = HttpContext.Current.User.Identity.Name;
+            if (string.IsNullOrEmpty(openId))
+            {
+                return new ResponseModel
+                {
+                    ErrorCode = 0,
+                    Error = true
+                };
+            }
+            else
+            {
+                return new ResponseModel
+                {
+                    ErrorCode = 0,
+                    Error = false
+                };
+
+            }            
+        }
+    }
+}
